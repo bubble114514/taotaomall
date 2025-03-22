c9f51cde191f76b40bb8c3b08374e9352c43caa5
泡泡子 <13610223+bubble-seed@user.noreply.gitee.com>
2025-03-22 19:05:33 +0800

    统一异常处理+JSR303校验

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -19,6 +19,27 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *  1)、配置全局巨大逻辑删除规则
  *  2)、配置逻辑删除的组件（MybatisPlus 3.1.0之后的版本可以不配置）
  *  3）、在Bean上添加@TableLogic逻辑删除注解
+ *
+ * 3、JSR303
+ *  1)、给Bean添加校验注解：javax.validation.constraints.*，并定义自己的message提示
+ *  2)、在对应Controller类开启校验功能@Valid
+ *      效果：校验错误以后会有默认的响应，返回给前端
+ *  3）、给校验的bean后紧跟一个BindingResult，就可以获取到校验的错误信息
+ *  4）、分组校验 (多场景的复杂校验）
+ *      1)@NotBlank(message = "品牌名不能为空", groups = {AddGroup.class, UpdateGroup.class})
+ *      给校验注释标注蛇魔情况需要进行校验
+ *      2)@Validated({AddGroup.class})
+ *      给方法标注，实现指定分组校验功能
+ *      3)默认没有制定分组的校验注解@NotBlank，在分组校验情况下不生效，只会在@Validated(AddGroup.class)生效
+ *  5）、自定义校验
+ *    1)、编写一个自定义校验注解
+ *    2)、编写一个自定义校验器@Component
+ *    3)、关联自定义校验注解和自定义校验器
+ *      @Documented
+ *      @Constraint(validatedBy = {ListValueConstraintValidator.class})【可以指定多个不同的校验器】
+ *      @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+ *      @Retention(RetentionPolicy.RUNTIME)
+ * 4.统一异常处理Exception类
  */
 
 
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
@@ -1,10 +1,15 @@
 package com.atguigu.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 
+import com.atguigu.common.valid.AddGroup;
+import com.atguigu.common.valid.UpdateStatusGroup;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +21,7 @@ import com.atguigu.gulimall.product.service.BrandService;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.R;
 
+import javax.validation.Valid;
 
 
 /**
@@ -56,9 +62,23 @@ public class BrandController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
-
+    public R save(@Validated({AddGroup.class}) @RequestBody BrandEntity brand /*, BindingResult result*/){
+//        if (result.hasErrors()){
+//            Map<String,String> map=new HashMap<>();
+//            //1、获取校验的错误结果
+//            result.getFieldErrors().forEach(item->{
+//                //FieldError
+//                String message=item.getDefaultMessage();
+//                //获取错误的属性的名字
+//                String field=item.getField();
+//                map.put(field,message);
+//            });
+//
+//            return R.error(400,"提交的数据不合法").put("data",map);
+//        }else {
+//
+//        }
+        brandService.save(brand);
         return R.ok();
     }
 
@@ -71,16 +91,14 @@ public class BrandController {
 
         return R.ok();
     }
-//    /**
-//     * 修改显示状态
-//     */
-//    @RequestMapping("/update/status")
-//    public R updateStatus(@RequestParam Long   brandId,
-//                          @RequestParam Integer  showStatus){
-//		brandService.updateStatus(brandId,showStatus);
-//
-//        return R.ok();
-//    }
+    /**
+     * 修改显示状态
+     */
+    @RequestMapping("/update/status")
+    public R updateStatus(@Validated({UpdateStatusGroup.class}) @RequestBody BrandEntity brand){
+		brandService.updateById(brand);
+        return R.ok();
+    }
 
     /**
      * 删除
