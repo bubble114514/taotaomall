673f6bb3653b5f41ec7b15992e89aa6c23208bb7
泡泡子 <13610223+bubble-seed@user.noreply.gitee.com>
2025-04-08 20:56:16 +0800

    检索返回结果模型

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/service/MallSearchService.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/service/MallSearchService.java
@@ -1,9 +1,10 @@
 package com.atguigu.gulimall.search.service;
 
 import com.atguigu.gulimall.search.vo.SearchParam;
+import com.atguigu.gulimall.search.vo.SearchResult;
 
 public interface MallSearchService {
 
-    Object search(SearchParam param);
+    SearchResult search(SearchParam param);
 
 }
